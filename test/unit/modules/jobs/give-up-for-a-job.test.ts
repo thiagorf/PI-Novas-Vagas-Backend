87b3f2b1d009a39dc96719bc14177c4298b1b4c9
thiagorf <trfarciroli@gmail.com>
2022-07-16 18:38:11 -0300

    test: give up for a job tests

--- test/unit/modules/jobs/give-up-for-a-job.test.ts
+++ test/unit/modules/jobs/give-up-for-a-job.test.ts
@@ -1,4 +1,4 @@
-import { ApplyForAJobUseCase } from "../../../../src/modules/jobs/core/useCases/applyforAjob/apply-for-a-job-use-case";
+import { GiveUpForAJobUseCase } from "../../../../src/modules/jobs/core/useCases/giveUpForAJob/give-up-for-a-job-use-case";
 import { ApplicantBuilder } from "../../builders/applicant-builder";
 import { JobsBuilder } from "../../builders/jobs-builder";
 import { UsersBuilder } from "../../builders/users-builder";
@@ -11,59 +11,18 @@ const prepareUseCase = () => {
     const inMemoryJobsRepository = new InMemoryJobsRepository();
     const inMemoryApplyRepository = new InMemoryApplyRepository();
 
-    const applyForAJob = new ApplyForAJobUseCase(
-        inMemoryApplicantRepository,
+    const giveUpForAJob = new GiveUpForAJobUseCase(
         inMemoryJobsRepository,
+        inMemoryApplicantRepository,
         inMemoryApplyRepository,
     );
 
-    return { applyForAJob, inMemoryApplicantRepository, inMemoryJobsRepository, inMemoryApplyRepository };
+    return { giveUpForAJob, inMemoryApplicantRepository, inMemoryJobsRepository, inMemoryApplyRepository };
 };
 
-describe("Apply For A Job", () => {
-    it("Should be able to apply to a job", async () => {
-        const { applyForAJob, inMemoryApplicantRepository, inMemoryJobsRepository } = prepareUseCase();
-
-        const userBuilder = UsersBuilder.aUser().build();
-        const applicantBuilder = ApplicantBuilder.aApplicant().withUserId(userBuilder.id).build();
-        const jobsBuilder = JobsBuilder.aJob().build();
-
-        const applicantData = { ...userBuilder, ...applicantBuilder };
-
-        const applicant = await inMemoryApplicantRepository.createAnApplicant(applicantData);
-        const job = await inMemoryJobsRepository.createANewJob(jobsBuilder);
-
-        const sut = await applyForAJob.perform({ jobs_id: job.id, user_id: applicant.user_id });
-
-        expect(sut).toHaveProperty("created_at");
-        expect(sut).toHaveProperty("id", 0);
-    });
-
-    it("Should not be able to apply to a job with an invalid applicant", () => {
-        const { applyForAJob } = prepareUseCase();
-
-        expect(async () => {
-            await applyForAJob.perform({ jobs_id: 0, user_id: 0 });
-        }).rejects.toThrowError("Invalid applicant");
-    });
-
-    it("Should not be able to apply to a job with an invalid job", async () => {
-        const { applyForAJob, inMemoryApplicantRepository } = prepareUseCase();
-
-        const userBuilder = UsersBuilder.aUser().build();
-        const applicantBuilder = ApplicantBuilder.aApplicant().withUserId(userBuilder.id).build();
-
-        const applicantData = { ...userBuilder, ...applicantBuilder };
-
-        const applicant = await inMemoryApplicantRepository.createAnApplicant(applicantData);
-
-        expect(async () => {
-            await applyForAJob.perform({ user_id: applicant.user_id, jobs_id: 0 });
-        }).rejects.toThrowError("Invalid job.");
-    });
-
-    it("Should not be able to apply to a job that has already been applied", async () => {
-        const { applyForAJob, inMemoryApplicantRepository, inMemoryApplyRepository, inMemoryJobsRepository } =
+describe("Give Up For A Job", () => {
+    it("Should be able to give up for a job", async () => {
+        const { giveUpForAJob, inMemoryApplicantRepository, inMemoryJobsRepository, inMemoryApplyRepository } =
             prepareUseCase();
 
         const userBuilder = UsersBuilder.aUser().build();
@@ -77,8 +36,57 @@ describe("Apply For A Job", () => {
 
         await inMemoryApplyRepository.apply(applicant.id, job.id);
 
+        const sut = await giveUpForAJob.perform({ jobs_id: job.id, user_id: applicant.user_id });
+
+        expect(sut).toHaveProperty("created_at");
+        expect(sut).toHaveProperty("id", 0);
+    });
+
+    it("Should not be able to give up with an invalid user", () => {
+        const { giveUpForAJob } = prepareUseCase();
+
         expect(async () => {
-            await applyForAJob.perform({ jobs_id: job.id, user_id: applicant.user_id });
-        }).rejects.toThrowError("User already applied.");
+            await giveUpForAJob.perform({ user_id: 0, jobs_id: 0 });
+        }).rejects.toThrowError("Invalid applicant");
+    });
+
+    it("Should not be able to give up with a invalid job", async () => {
+        const { giveUpForAJob, inMemoryApplicantRepository, inMemoryJobsRepository, inMemoryApplyRepository } =
+            prepareUseCase();
+
+        const invalidJobId = 1;
+
+        const userBuilder = UsersBuilder.aUser().build();
+        const applicantBuilder = ApplicantBuilder.aApplicant().withUserId(userBuilder.id).build();
+        const jobsBuilder = JobsBuilder.aJob().build();
+
+        const applicantData = { ...userBuilder, ...applicantBuilder };
+
+        const applicant = await inMemoryApplicantRepository.createAnApplicant(applicantData);
+        const job = await inMemoryJobsRepository.createANewJob(jobsBuilder);
+
+        await inMemoryApplyRepository.apply(applicant.id, job.id);
+
+        expect(async () => {
+            await giveUpForAJob.perform({ jobs_id: invalidJobId, user_id: applicant.user_id });
+        }).rejects.toThrowError("Invalid job.");
+    });
+
+    it("Should not be able to give up a job that is not applied", async () => {
+        const { giveUpForAJob, inMemoryApplicantRepository, inMemoryJobsRepository, inMemoryApplyRepository } =
+            prepareUseCase();
+
+        const userBuilder = UsersBuilder.aUser().build();
+        const applicantBuilder = ApplicantBuilder.aApplicant().withUserId(userBuilder.id).build();
+        const jobsBuilder = JobsBuilder.aJob().build();
+
+        const applicantData = { ...userBuilder, ...applicantBuilder };
+
+        const applicant = await inMemoryApplicantRepository.createAnApplicant(applicantData);
+        const job = await inMemoryJobsRepository.createANewJob(jobsBuilder);
+
+        expect(async () => {
+            await giveUpForAJob.perform({ jobs_id: job.id, user_id: applicant.user_id });
+        }).rejects.toThrowError("Invalid applicant");
     });
 });
