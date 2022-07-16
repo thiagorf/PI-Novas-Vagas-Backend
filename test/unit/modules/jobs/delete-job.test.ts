9598847dba9767597e08e92698359ac81678c15a
thiagorf <trfarciroli@gmail.com>
2022-07-16 11:42:27 -0300

    test: delete job tests

---
+++ test/unit/modules/jobs/delete-job.test.ts
@@ -0,0 +1,39 @@
+import { DeleteJobUseCase } from "../../../../src/modules/jobs/core/useCases/deleteJob/delete-job-use-case";
+import { JobsBuilder } from "../../builders/jobs-builder";
+import { InMemoryJobsRepository } from "../../inMemory/in-memory-jobs-repository";
+
+const prepareUseCase = () => {
+    const inMemoryJobsRepository = new InMemoryJobsRepository();
+    const deleteJob = new DeleteJobUseCase(inMemoryJobsRepository);
+
+    return { deleteJob, inMemoryJobsRepository };
+};
+
+describe("Delete Job", () => {
+    it("Should be able to delete a job", async () => {
+        const { deleteJob, inMemoryJobsRepository } = prepareUseCase();
+
+        const jobsBuilder = JobsBuilder.aJob().build();
+
+        for (let i = 0; i < 2; i++) {
+            await inMemoryJobsRepository.createANewJob(jobsBuilder);
+        }
+
+        const allJobs = await inMemoryJobsRepository.getAllJobs();
+
+        const sut = await deleteJob.perform(allJobs[0].id);
+
+        const match = allJobs.find((job) => job.id === sut.id);
+
+        expect(allJobs).toHaveLength(1);
+        expect(match).toBeUndefined();
+    });
+
+    it("Should not be able to delete an inexisting job", () => {
+        const { deleteJob } = prepareUseCase();
+
+        expect(async () => {
+            await deleteJob.perform(0);
+        }).rejects.toThrowError("Invalid job or inexisting");
+    });
+});
