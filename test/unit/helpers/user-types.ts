bdda61db665d13252d05bd049a465b44fa6b25fe
thiagorf <trfarciroli@gmail.com>
2022-07-17 11:21:46 -0300

    test: get one enterprise tests

--- test/unit/helpers/user-types.ts
+++ test/unit/helpers/user-types.ts
@@ -1,12 +1,29 @@
+import { Jobs } from "../../../src/modules/jobs/core/entity/Jobs";
 import { ApplicantJobs, Applicants } from "../../../src/modules/users/core/entity/Applicants";
+import { Enterprise } from "../../../src/modules/users/core/entity/Enterprises";
 
 type UserData = {
-    name: string;
-    email: string;
-    password: string;
+    id?: number;
+    name?: string;
+    email?: string;
+    password?: string;
+    type?: "applicant" | "enterprise";
 };
 
 export interface ApplicantInformation extends Applicants {
     user?: UserData;
     jobs?: ApplicantJobs[];
+}
+
+export interface EnterpriseInformation extends Enterprise {
+    user?: UserData;
+    jobs?: ApplicantJobs[];
+}
+
+export interface JobsApplicants extends Jobs {
+    applicants: {
+        curriculum: string;
+        photo: string;
+        user: UserData;
+    }[];
 }
--- test/unit/inMemory/in-memory-enterprise-repository.ts
+++ test/unit/inMemory/in-memory-enterprise-repository.ts
@@ -1,16 +1,35 @@
-import { Enterprise } from "../../../src/modules/users/core/entity/Enterprises";
+import { Enterprise, EnterpriseAllJobs, EnterpriseJobs } from "../../../src/modules/users/core/entity/Enterprises";
 import { CreateEnterpriseDTO } from "../../../src/modules/users/core/useCases/enterprises/createEnterprise/create-enterprise-dto";
 import { EnterpriseRepository } from "../../../src/modules/users/infra/repositories/enterprise-repository";
+import { EnterpriseInformation, JobsApplicants } from "../helpers/user-types";
 
 export class InMemoryEnterpriseRepository implements EnterpriseRepository {
     private id = 0;
-    private enterprise: Enterprise[] = [];
+    private enterprise: EnterpriseInformation[] = [];
+    private jobs: EnterpriseJobs[] = [];
+    private jobsWithApplicants: JobsApplicants;
+
+    public populateJobs(jobs: EnterpriseJobs) {
+        this.jobs.push(jobs);
+    }
+
+    public populateJobsWithApplicants(data: JobsApplicants) {
+        this.jobsWithApplicants = data;
+    }
 
     async createAnEnterprise(dto: CreateEnterpriseDTO): Promise<Enterprise> {
-        const enterprise: Enterprise = {
+        const { email, name, password } = dto;
+
+        const enterprise: EnterpriseInformation = {
             id: this.id,
             user_id: this.id,
             ...dto,
+            user: {
+                name,
+                email,
+                type: "enterprise",
+                password,
+            },
         };
 
         this.enterprise.push(enterprise);
@@ -30,16 +49,44 @@ export class InMemoryEnterpriseRepository implements EnterpriseRepository {
         return enterprise;
     }
 
-    getJobs(id: number): Promise<any> {
-        throw new Error("Method not implemented.");
+    async getJobs(id: number): Promise<any> {
+        const enterprise = this.enterprise.find((enterprise) => enterprise.id === id);
+
+        const { id: enterprise_id, cep, cnpj, segment, user_id } = enterprise;
+
+        const enterpriseJobs: EnterpriseAllJobs = {
+            id: enterprise_id,
+            cep,
+            cnpj,
+            segment,
+            user_id,
+            jobs: this.jobs,
+        };
+
+        return enterpriseJobs;
     }
-    getEnterpriseById(id: number): Promise<Enterprise> {
-        throw new Error("Method not implemented.");
+
+    async getEnterpriseById(id: number): Promise<Enterprise> {
+        const enterprise = this.enterprise.find((enterprise) => enterprise.id === id);
+
+        return enterprise;
     }
-    getAllEnterpriseInfo(enterprise_id: number): Promise<Enterprise> {
-        throw new Error("Method not implemented.");
+
+    async getAllEnterpriseInfo(enterprise_id: number): Promise<Enterprise> {
+        const enterprise = this.enterprise.find((enterprise) => enterprise.id === enterprise_id);
+
+        return enterprise;
     }
-    getOneJobForEnterprise(enterprise_id: number, job_id: number): Promise<any> {
-        throw new Error("Method not implemented.");
+
+    // TODO TEST
+    async getOneJobForEnterprise(enterprise_id: number, job_id: number): Promise<any> {
+        const job = this.jobs.find((job) => job.id === job_id);
+
+        const enterpriseOneJob: JobsApplicants = {
+            ...job,
+            applicants: [],
+        };
+
+        return enterpriseOneJob;
     }
 }
