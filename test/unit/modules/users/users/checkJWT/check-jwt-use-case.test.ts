b3f65605cd4edb3aadf4197d175a3708deb0d2c1
thiagorf <trfarciroli@gmail.com>
2022-07-20 11:02:07 -0300

    setup users tests

---
+++ test/unit/modules/users/users/checkJWT/check-jwt-use-case.test.ts
@@ -0,0 +1,6 @@
+describe("Check JWT", () => {
+    test.todo("Should be able to check jwt");
+    test.todo("Should not be able to check a empty token");
+    test.todo("Should not be able to pass an invalid token");
+    test.todo("Should not be able to pass a token with a inexisting user");
+});
---
+++ test/unit/modules/users/users/userAuth/user-auth-use-case.test.ts
@@ -0,0 +1,6 @@
+describe("User Auth", () => {
+    test.todo("Should be able to authenticate an user, when is it an applicant");
+    test.todo("Should be able to authenticate an user, when is it an enterprise");
+    test.todo("Should not be able to authenticate an invalid user");
+    test.todo("Should not be able to authenticate an user with a wrong password");
+});
